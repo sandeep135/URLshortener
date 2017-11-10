f65497ca7c06e0f2e51f883152fe2126cb683c9b
sandeep135 <sandeepbhandari135@gmail.com>
2017-11-10 18:19:35 +0545

    first commit

---
+++ shortener/views.py
@@ -0,0 +1,63 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
+from analytics.models import ClickEvent
+
+from .forms import SubmitUrlForm
+from .models import KirrURL
+
+# Create your views here.
+
+def home_view_fbv(request, *args, **kwargs):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, "shortener/home.html", {})
+
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        the_form = SubmitUrlForm()
+        bg_image = 'https://fthmb.tqn.com/tZAtW8NcHj5HXTFMvAjqFT0Z3z4=/735x0/about/GettyImages-186843569-57ad20ce3df78cd39cba2be6.jpg'
+        context = {
+            "title": "SHORTLY.CO",
+            "form": the_form,
+            "bg_image": bg_image
+        }
+        return render(request, "shortener/home.html", context)  
+
+    def post(self, request, *args, **kwargs):
+        form = SubmitUrlForm(request.POST)
+        context = {
+            "title": "Shortly-URL shortener",
+            "form": form
+        }
+        template = "shortener/home.html"
+        if form.is_valid():
+            new_url = form.cleaned_data.get("url")
+            obj, created = KirrURL.objects.get_or_create(url=new_url)
+            context = {
+                "object": obj,
+                "created": created,
+            }
+            if created:
+                template = "shortener/success.html"
+            else:
+                template = "shortener/already-exists.html"
+    
+        return render(request, template ,context)
+
+
+class URLRedirectView(View):
+    def get(self, request, shortcode=None, *args, **kwargs):
+        qs = KirrURL.objects.filter(shortcode__iexact=shortcode)
+        if qs.count() != 1 and not qs.exists():
+            raise Http404
+        obj = qs.first()
+        print(ClickEvent.objects.create_event(obj))
+        return HttpResponseRedirect(obj.url)
+        
+
+
+
+
